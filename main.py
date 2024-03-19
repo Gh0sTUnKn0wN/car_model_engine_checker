45cb8785083fd56290a9a99de6051ec92945aff3
Gh0sTUnKn0wN <101837461+Gh0sTUnKn0wN@users.noreply.github.com>
2024-03-19 12:38:53 +0200

    Main Code

--- main.py
+++ main.py
@@ -1,3 +1,18 @@
-from variables import code
-print("Hello world!")
-code()
+import httpx
+import bs4
+from variables import status_code, prompt
+
+
+URL = "https://www.auto-data.net/en/results?search={search}"
+
+if __name__ == "__main__":
+    prompt()
+    search = str(input()) 
+    with httpx.Client() as client:
+        resp = client.get(URL.format(search=search))
+        
+        if resp.status_code == 200:
+            soup = bs4.BeautifulSoup(resp.text, features="lxml")
+            
+            for i in soup.find_all("span", {"class": "content"}):
+                print(i.text)
--- variables.py
+++ variables.py
@@ -1,2 +1,7 @@
-def code():
-    print("Hello there!")
+def status_code():
+    code = 200
+    if code != 200:
+        print("Error! Program failed")
+
+def prompt():
+    print_prompt = print("Enter vehicle modification you want to search for:  ")
