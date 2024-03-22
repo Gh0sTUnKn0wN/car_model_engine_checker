3ef4acdf23daedc4386e5c6fe16dbfe50a575f43
Gh0sTUnKn0wN <101837461+Gh0sTUnKn0wN@users.noreply.github.com>
2024-03-22 12:09:07 +0200

    Updated

--- main.py
+++ main.py
@@ -1,25 +1,34 @@
 import httpx
 import bs4
-from variables import status_code, prompt
+from variables import status_code, prompt, check_for_loop_done, check_status_code
 
 
 URL = "https://www.auto-data.net/en/results?search={search}"
+
+def result_save_done():
+    result_save.write("Output saved" + "\n") 
 
 if __name__ == "__main__":
     result_save = open("Old_results.txt" ,"a")
     if result_save == FileNotFoundError:
         open("Old_results.txt", "a")
     prompt()
+    
     search = str(input()) 
     with httpx.Client() as client:
         resp = client.get(URL.format(search=search))
-        
+        error_message = check_status_code(resp)
+        if error_message:
+            print("Error with collecting site code(200)!")
         if resp.status_code == status_code():
             soup = bs4.BeautifulSoup(resp.text, features="lxml")
             
+            result_list = []
+            
             for i in soup.find_all("span", {"class": "content"}):
                 result_save.write(search + " = " + i.text + "\n")
+                result_list.append(i.text)
                 print(i.text)
-            
-            result_save.write("Output saved" + "\n")
                 
+            iterator = iter(result_list)
+            check_for_loop_done(iterator, result_save_done())
--- variables.py
+++ variables.py
@@ -3,3 +3,15 @@ def status_code():
 
 def prompt():
     print_prompt = print("Enter vehicle modification you want to search for:  ")
+
+def check_for_loop_done(iterable, action):
+    try:
+        next(iterable)
+    except StopIteration:
+        action()
+
+def check_status_code(resp):
+    if resp.status_code != 200:
+        return f"Error: Response status code is {resp.status_code}."
+    else:
+        return ""
